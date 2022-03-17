5c0ac4bd731e649cccda40c6d001c94d51a55faa
gautambisht709 <101648317+e8c2cadf2149@users.noreply.github.com>
2022-03-17 14:07:49 +0530

    Update ig_bot.py

--- ig_bot.py
+++ ig_bot.py
@@ -79,17 +79,20 @@ def follow_like_comment():
         while count <2:
             search_2.send_keys(Keys.ENTER)
             count +=1 # count = count +1
-            time.sleep(1)
-        driver.implicitly_wait(30)
-        follow = driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/header/section/div[1]/div[2]/div/div/div/span/span[1]/button')
-        follow.click()
-        time.sleep(8)
-        unfollow1 = driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/header/section/div[1]/div[2]/div/div[2]/div/span/span[1]/button')
-        unfollow1.click()
-        driver.implicitly_wait(30)
-        unfollow2 = driver.find_element_by_xpath('/html/body/div[6]/div/div/div/div[3]/button[1]')
-        driver.implicitly_wait(30)
-        unfollow2.click()
+           driver.implicitly_wait(30)
+      
+           time.sleep(1)
+           driver.implicitly_wait(30)
+           follow = driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/header/section/div[1]/div[2]/div/div/div/span/span[1]/button')
+           follow.click()
+           time.sleep(15)
+           unfollow1 = driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/header/section/div[1]/div[2]/div/div[2]/div/span/span[1]/button')
+           driver.implicitly_wait(30)
+           unfollow1.click()
+           unfollow2 = driver.find_element_by_xpath('/html/body/div[6]/div/div/div/div[3]/button[1]')
+           driver.implicitly_wait(30)
+           unfollow2.click()
+        
         
 login('testuseraccount24', 'testuser')
 follow_like_comment()
