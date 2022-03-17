474731f8f4d47231a7e940d12a2539c7037a03fd
gautambisht709 <101648317+e8c2cadf2149@users.noreply.github.com>
2022-03-17 11:12:56 +0530

    Update ig_bot.py

--- ig_bot.py
+++ ig_bot.py
@@ -4,11 +4,11 @@ from selenium import webdriver
 import os
 
 chrome_options = webdriver.ChromeOptions()
-chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.binary_location = os.environ.get("/app/.apt/usr/bin/google-chrome")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+driver = webdriver.Chrome(executable_path=os.environ.get("/app/.chromedriver/bin/chromedriver"), chrome_options=chrome_options)
 
 # Now you can start using Selenium
 
