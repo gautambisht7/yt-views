ea4811ca794b1bbc44b41f0f414f58feb6588e25
gautambisht709 <101648317+e8c2cadf2149@users.noreply.github.com>
2022-03-21 08:45:58 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,7 @@
 from selenium import webdriver
 import time
-from random import seed
-from random import randint
 from selenium.webdriver.common.keys import Keys
-import os
+
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
@@ -11,87 +9,35 @@ chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-driver.maximize_window()
-class InstaBot:
-    def __init__(self, username, password):
-        driver.implicitly_wait(30)
-        driver.get("https://instagram.com")      
-        time.sleep(getRandomTime())
-        driver.find_element_by_xpath("//input[@name=\"username\"]")\
-            .send_keys('_gautambisht_11')
-        driver.find_element_by_xpath("//input[@name=\"password\"]")\
-            .send_keys('e8c2cadf2148')
-        time.sleep(getRandomTime())      
-        driver.find_element_by_xpath('//button[@type="submit"]')\
-            .click()
 
+driver.implicitly_wait(30)
+driver.get("https://instagram.com/virat.kohli/") 
+driver.implicitly_wait(30)
+login = driver.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[3]/div/span/a[1]/button').click()
+driver.implicitly_wait(30)
+time.sleep(3)
+username = driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[1]/div/label/input')
+username.click()
+username.send_keys('_gautambisht_11')
+time.sleep(1)
+password = driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[2]/div/label/input')
+password.click()
+time.sleep(1)
+password.send_keys('e8c2cadf2148')
+subit = driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[3]/button').click()
+time.sleep(5)
+driver.implicitly_wait(30)
+noti = driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/div/div/div/button').click()
+time.sleep(5)
+driver.implicitly_wait(30)
+while True: 
 
-        try:
-             driver.implicitly_wait(10)
-             driver.find_element_by_xpath('//button[@type="submit"]').click()
-     
-        except:
-             pass
-            
-
-        try:
-             
-             driver.implicitly_wait(10)
-             driver.find_element_by_xpath('//button[@type="submit"]').click()
-        
-
-        except:
-           pass
-        
-        try:
-            driver.implicitly_wait(30)
-            time.sleep(getRandomTime())
-            search_2 = driver.find_element_by_xpath('//input[@placeholder="Search"]')
-            search_2.send_keys('virat.kohli')
-            time.sleep(6)
-            search_2.send_keys(Keys.ENTER)   
-            count = 0
-            while count <2:
-                search_2.send_keys(Keys.ENTER)
-                count +=1 # count = count +1
-                time.sleep(1)
-        except:
-            driver.refresh()
-            time.sleep(getRandomTime())
-            search_2 = driver.find_element_by_xpath('//input[@placeholder="Search"]')
-            search_2.send_keys('virat.kohli')
-            time.sleep(6)
-            search_2.send_keys(Keys.ENTER)   
-            count = 0
-            while count <2:
-                search_2.send_keys(Keys.ENTER)
-                count +=1 # count = count +1
-                time.sleep(1)
-        time.sleep(getRandomTime())
-        driver.implicitly_wait(30)
-        follow = driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/header/section/div[1]/div[2]/div/div/div/span/span[1]/button')
-def getRandomTime():
-        randTime = randint(3, 5)
-        return randTime      
-InstaBot("your_usernmae", "your_password")   
-
-while (True):  
-    try:
-        follow = driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/header/section/div[1]/div[2]/div/div/div/span/span[1]/button').click()
-        print('Following')
-        time.sleep(3)
-        driver.implicitly_wait(30)
-    except:
-        pass
-    try:
-        unfollow1 = driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/header/section/div[1]/div[2]/div/div[2]/div/span/span[1]/button').click()
-        time.sleep(1)
-        unfollow2 = driver.find_element_by_xpath('/html/body/div[6]/div/div/div/div[3]/button[1]').click()
-        print('Unfollowed')
-        time.sleep(2)
-        driver.refresh()
-        print("Refreshing.")
-        time.sleep(5)
-    except:
-        pass
-        
+    driver.implicitly_wait(30)
+    follow = driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/header/section/div[1]/div[2]/div/div/div/span/span[1]/button').click()
+    time.sleep(20)
+    unfollow1 = driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/header/section/div[1]/div[2]/div/div[2]/div/span/span[1]/button').click()
+    time.sleep(2)
+    unfollow2 = driver.find_element_by_xpath('/html/body/div[6]/div/div/div/div[3]/button[1]').click()
+    print('unfollowing')
+    time.sleep(6)
+    
