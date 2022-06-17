676fda0543f0c23cd125022f6f76d7585c748261
gautambisht709 <101648317+e8c2cadf2149@users.noreply.github.com>
2022-06-16 22:04:03 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,11 +6,9 @@ from random import seed
 from random import randint
 
 chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("--incognito")
+
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--no-sandbox")
+
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
 while True:
@@ -29,6 +27,6 @@ while True:
      driver.execute_script('''window.open("https://www.youtube.com/watch?v=LVYMwW79j6Q", "_blank");''')
      time.sleep(66)
      print('Done')
-     browser.quit()
+     driver.quit()
      time.sleep(5)
 
