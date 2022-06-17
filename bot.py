95fd3642962b7847f4fc13477768bb4dd1a176c1
gautambisht709 <101648317+e8c2cadf2149@users.noreply.github.com>
2022-06-16 22:17:39 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,19 +12,9 @@ chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
 while True:
+     print('Starting')
      driver.get('''https://www.youtube.com/watch?v=LVYMwW79j6Q''')
-     time.sleep(5)
-     driver.execute_script('''window.open("https://www.youtube.com/watch?v=LVYMwW79j6Q", "_blank");''')
-     time.sleep(5)
-     driver.execute_script('''window.open("https://www.youtube.com/watch?v=LVYMwW79j6Q", "_blank");''')
-     time.sleep(5)
-     driver.execute_script('''window.open("https://www.youtube.com/watch?v=LVYMwW79j6Q", "_blank");''')
-     time.sleep(5)
-
-     driver.execute_script('''window.open("https://www.youtube.com/watch?v=LVYMwW79j6Q", "_blank");''')
-     time.sleep(5)
-     
-     driver.execute_script('''window.open("https://www.youtube.com/watch?v=LVYMwW79j6Q", "_blank");''')
+     print('Started')
      time.sleep(66)
      print('Done')
      driver.quit()
