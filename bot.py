0465e751871042d76ac9a67a38e0dc7fe31e471e
gautambisht709 <101648317+e8c2cadf2149@users.noreply.github.com>
2022-06-17 08:46:04 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-driver.get('''https://www.youtube.com/watch?v=LVYMwW79j6Q''')
+
 while True:
      driver.get('''https://www.youtube.com/watch?v=LVYMwW79j6Q''')
      time.sleep(8)
