eaa0350dcc7ef09de69b9f3b800c54a289a84422
gautambisht709 <101648317+gautambisht7@users.noreply.github.com>
2023-07-14 22:40:41 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,34 +1,38 @@
-from selenium import webdriver
 import time
-from selenium.webdriver.common.keys import Keys
-import os
-
-from random import seed
-from random import randint
-
-from webdriver_manager.chrome import ChromeDriverManager
-driver = webdriver.Chrome(ChromeDriverManager().install())
-chrome_options = webdriver.ChromeOptions()
-chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--no-sandbox")
-def getRandomTime():
-        randTime = randint(72, 90)
-        return randTime 
-def getRandomime():
-        randTime1 = randint(5, 10)
-        return randTime1
-
-driver.get('''https://www.youtube.com/results?search_query=Nxt+Level+Gaming+CRIMINALS+VS+HIPHOP+%7C%7C+FREE+FIRE+3D+ANIMATION''')
-time.sleep(getRandomime())
-a=driver.find_element_by_xpath('//*[@id="dismissible"]/div')
-a.click()
-
 while True:
-    try: 
-       time.sleep(getRandomTime())
-       driver.refresh()
-       driver.get('https://www.youtube.com/watch?v=LVYMwW79j6Q')
-    except :
-        pass
+   print('a')
+   time.sleep(5)
+# from selenium import webdriver
+# import time
+# from selenium.webdriver.common.keys import Keys
+# import os
+
+# from random import seed
+# from random import randint
+
+# from webdriver_manager.chrome import ChromeDriverManager
+# driver = webdriver.Chrome(ChromeDriverManager().install())
+# chrome_options = webdriver.ChromeOptions()
+# chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+# chrome_options.add_argument("--headless")
+# chrome_options.add_argument("--disable-dev-shm-usage")
+# chrome_options.add_argument("--no-sandbox")
+# def getRandomTime():
+#         randTime = randint(72, 90)
+#         return randTime 
+# def getRandomime():
+#         randTime1 = randint(5, 10)
+#         return randTime1
+
+# driver.get('''https://www.youtube.com/results?search_query=Nxt+Level+Gaming+CRIMINALS+VS+HIPHOP+%7C%7C+FREE+FIRE+3D+ANIMATION''')
+# time.sleep(getRandomime())
+# a=driver.find_element_by_xpath('//*[@id="dismissible"]/div')
+# a.click()
+
+# while True:
+#     try: 
+#        time.sleep(getRandomTime())
+#        driver.refresh()
+#        driver.get('https://www.youtube.com/watch?v=LVYMwW79j6Q')
+#     except :
+#         pass
